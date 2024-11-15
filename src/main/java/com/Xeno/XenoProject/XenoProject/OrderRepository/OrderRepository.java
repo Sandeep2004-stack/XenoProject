793c5abea10971eed6f15cf60424704b2d63b286
Sanjay Vikram M <sanjayvikram.m005@gmail.com>
2024-11-15 13:18:23 +0530

    moved folders

--- src/main/java/com/Xeno/XenoProject/XenoProject/OrderRepository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.Xeno.XenoProject.XenoProject.OrderRepository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.Xeno.XenoProject.XenoProject.order.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-	
-}
